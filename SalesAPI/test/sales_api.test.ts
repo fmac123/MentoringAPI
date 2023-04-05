51805d83a4916c5ebaca7566cb73d7973802d3b1
fraser <macaulay.fraser@gmail.com>
2023-04-06 09:29:16 +1000

    Initial commit

---
+++ SalesAPI/test/sales_api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SalesApi from '../stack/sales_api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sales_api-stack.ts
+// test('SQS Queue Created', () => {
+// //   const app = new cdk.App();
+// //     // WHEN
+// //   const stack = new SalesApi.SalesApiStack(app, 'MyTestStack');
+// //     // THEN
+// //   const template = Template.fromStack(stack);
+
+// //   template.hasResourceProperties('AWS::SQS::Queue', {
+// //     VisibilityTimeout: 300
+// //   });
+// });
